1194201166444dd50868e2e417808dde99ac1be1
shihab-5 <shihabuli364@gmail.com>
2026-08-16 23:04:31 +0600

    modify prisma.ts file

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"] || "postgresql://postgres:postgres@localhost:5432/postgres",
+    url: process.env["DATABASE_URL"],
   },
 });
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import dotenv from "dotenv";
 
-// Load environment variables
 dotenv.config();
 
-// Ensure DATABASE_URL is defined
 const DATABASE_URL =
   process.env.DATABASE_URL ||
   "postgresql://postgres:postgres@localhost:5432/postgres";
 
-// Create adapter with PostgreSQL connection
 const adapter = new PrismaPg({ connectionString: DATABASE_URL });
 
-// // Create and export Prisma client
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
