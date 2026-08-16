eccfdc5a6029379503a634cd1a9f73ad8874a1d7
shihab-5 <shihabuli364@gmail.com>
2026-08-16 16:27:32 +0600

    Remove tracked dist directory and add to .gitignore

--- dist/scripts/createCategory.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=createCategory.d.ts.map
--- dist/services/users.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-// Initialize Express router for user-related routes
-const router = (0, express_1.Router)();
-//# sourceMappingURL=users.js.map
